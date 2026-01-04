a42a7c8f8c28ddd6fa0c7046397bf0310ad9e369
TwinkyCasidsid <twinkycasidsidx@gmail.com>
2026-01-04 21:35:15 +0800

    feat: add gh-pages dependency and update Navbar component to use Link from react-router-dom
    - Added "gh-pages" as a dependency in package.json.
    - Updated Navbar component to replace anchor tags with Link components for navigation.
    - Changed main.jsx to use HashRouter instead of BrowserRouter for routing.
    - Minor formatting adjustments in the Navbar component and main.jsx.
    - Updated vite.config.js to ensure base path is correctly set.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,25 @@
+import { Link } from "react-router-dom";
 import winxLogo from "/winx.png";
 
 export default function Navbar() {
   return (
     <div className="h-15 fixed top-5 left-0 right-0 z-50 flex justify-center">
-      <nav
-        className="flex items-center justify-center space-x-10
-        px-24 py-4 rounded-full
-        bg-[#6953a6]
-        text-white font-semibold
-        border border-white/20
-        shadow-[0_3px_5px_rgba(0,0,0,0.15)]
-        hover:shadow-[0_3px_20px_rgba(0,0,0,0.35)]
-        transition-all duration-300 backdrop-blur-xl"
-      >
-        <a href="/projects" className="hover:opacity-80">
-          Projects
-        </a>
+      <nav className="flex items-center justify-center space-x-10 px-24 py-4 rounded-full bg-[#6953a6] text-white font-semibold border border-white/20 shadow-[0_3px_5px_rgba(0,0,0,0.15)] hover:shadow-[0_3px_20px_rgba(0,0,0,0.35)] transition-all duration-300 backdrop-blur-xl">
+        <Link to="/projects" className="hover:opacity-80">Projects</Link>
 
         <span className="block w-px h-8 bg-white/40 rounded-full"></span>
 
-        <a href="/" className="hover:opacity-80">
+        <Link to="/" className="hover:opacity-80">
           <img
             src={winxLogo}
             className="h-12 w-auto opacity-95 drop-shadow-[0_0_6px_rgba(255,255,255,0.6)]"
             alt="winx logo"
           />
-        </a>
+        </Link>
 
         <span className="block w-px h-8 bg-white/40 rounded-full"></span>
 
-        <a href="/about" className="hover:opacity-80">
-          About
-        </a>
+        <Link to="/about" className="hover:opacity-80">About</Link>
       </nav>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/twinkyportfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
